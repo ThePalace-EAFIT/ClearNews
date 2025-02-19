fef6131e1c4bfd26a1225eacf61168db4dd55e75
Velezinho24 <svelezb6@eafit.edu.co>
2025-02-19 03:38:19 -0500

    changes in the search

--- Search/models.py
+++ Search/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
 # Create your models here.
+
+
+class New(models.Model):
+    headline = models.CharField(max_length=100)
+    body = models.CharField(max_length=250)
+    date = models.DateTimeField(default=timezone.now)
+    url = models.URLField(blank=True)
+    credibility_score = models.IntegerField(
+        default=50,
+        validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
