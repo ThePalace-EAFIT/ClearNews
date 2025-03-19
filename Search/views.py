fbeea0373b1e92ea371dcdd6caaf1e323e3f6a7a
velezinho24 <svelezb6@eafit.edu.co>
2025-03-19 10:38:46 -0500

    New interface

--- Search/views.py
+++ Search/views.py
@@ -5,10 +5,11 @@ from .models import New
 
 
 def search_view(request):
-    # Obtiene la consulta de búsqueda si existe
     query = request.GET.get('q', '')
     return render(request, 'search.html', {'query': query})
 
+def search(request):
+    return render(request, 'about.html')
 
 def news_search(request):
     searchTerm = request.GET.get('searchNew')
@@ -20,20 +21,20 @@ def news_search(request):
     return render(request, 'search_results.html', {'searchTerm': searchTerm, 'name': 'Santiago', 'news': news})
 
 def loading_view(request):
-    query = request.GET.get("verifyNew", "").strip()  # Elimina espacios en blanco
+    query = request.GET.get("verifyNew", "").strip() 
 
     if query:
-        api_key = "AIzaSyB1FL2jEMWnoXW7BAsf4VYo3fWdtBsvvgY"  # Reemplázala con tu clave real
+        api_key = "AIzaSyB1FL2jEMWnoXW7BAsf4VYo3fWdtBsvvgY" 
         api_url = f"https://factchecktools.googleapis.com/v1alpha1/claims:search?query={query}&key={api_key}"
 
         response = requests.get(api_url)
 
         if response.status_code == 200:
             data = response.json()
-            print(f"Respuesta de la API: {data}")  # Verifica en la consola
+            print(f"Respuesta de la API: {data}")  
         else:
             data = {"error": f"Error {response.status_code}: {response.text}"}
-            print(data)  # Muestra el error en consola
+            print(data) 
 
         return render(request, "results.html", {"query": query, "data": data})
 
